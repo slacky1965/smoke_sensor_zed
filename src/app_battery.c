687bd48c74f84b5b91d5e0c38864f10544eae43b
Fedor <lfa1@yandex.ru>
2024-07-05 07:14:32 +0300

    Update app_battery.c

--- src/app_battery.c
+++ src/app_battery.c
@@ -3,6 +3,55 @@
 
 #include "app_main.h"
 
+#define VOLTAGE_FACTOR  120
+
+/*
+ * 9v--[200k]-----[100k]--
+ *             |         |
+ *            ADC       GND
+ *
+ *            3040-3060 - 9.0V
+ *            3010-3030 - 8.9V
+ *            2970-2990 - 8.8V
+ *            2940-2960 - 8.7V
+ *            2910-2930 - 8.6V
+ *            2870-2900 - 8.5V
+ *            2840-2860 - 8.4V
+ *            2800-2830 - 8.3V
+ *            2770-2790 - 8.2V
+ *            2740-2760 - 8.1V
+ *            2700-2730 - 8.0V
+ *            2670-2690 - 7.9V
+ *            2630-2660 - 7.8V
+ *            2600-2620 - 7.7V
+ *            2570-2590 - 7.6V
+ *            2530-2560 - 7.5V
+ *            2500-2520 - 7.4V
+ *            2470-2490 - 7.3V
+ *            2430-2460 - 7.2V
+ *            2400-2420 - 7.1V
+ *            2370-2390 - 7.0V
+ *            2330-2350 - 6.9V
+ *            2290-2310 - 6.8V
+ *            2250-2280 - 6.7V
+ *            2220-2240 - 6.6V
+ *            2190-2210 - 6.5V
+ *            2160-2180 - 6.4V
+ *            2120-2140 - 6.3V
+ *            2090-2110 - 6.2V
+ *            2050-2070 - 6.1V
+ *            2020-2040 - 6.0V
+ *            1990-2010 - 5.9V
+ *
+ *  VOLTAGE_FACTOR = 120
+ *
+ *  ADC  * 3 - VOLTAGE_FACTOR = 5...9V
+ *  2190 * 3 - VOLTAGE_FACTOR = 6450
+ *  2210 * 3 - VOLTAGE_FACTOR = 6510
+ */
+
+
+
 static void voltage_divisor_en(uint8_t on) {
     if (on) drv_gpio_write(VOLTAGE_DIVISOR_PIN_EN, ON);
     else drv_gpio_write(VOLTAGE_DIVISOR_PIN_EN, OFF);
@@ -21,15 +70,43 @@ static uint8_t get_battery_level(uint16_t battery_mv) {
 
 int32_t batteryCb(void *arg) {
 
+    uint16_t voltage_raw, voltage_9v;
+    uint8_t voltage, level;
+
+    uint16_t len;
+    epInfo_t dstEpInfo;
+    zoneStatusChangeNoti_t statusChangeNotification;
+
     voltage_divisor_en(ON);
     drv_adc_enable(ON);
 
-    uint16_t voltage_raw = drv_get_adc_data();
-    uint8_t voltage = (uint8_t)(voltage_raw/100);
-    uint8_t level = get_battery_level(voltage_raw);
+    voltage_raw = drv_get_adc_data();
+
+    drv_adc_enable(OFF);
+    voltage_divisor_en(OFF);
+
+    if ((voltage_raw * 3) < VOLTAGE_FACTOR) {
+        voltage_9v = 0;
+    } else {
+        voltage_9v = voltage_raw*3-VOLTAGE_FACTOR;
+    }
+
+    if (voltage_9v % 100 >= 50) {
+        voltage_9v /= 100;
+        voltage_9v++;
+        voltage = voltage_9v;
+        voltage_9v *= 100;
+    } else {
+        voltage_9v /= 100;
+        voltage = voltage_9v;
+        voltage_9v *= 100;
+    }
+
+    level = get_battery_level(voltage_9v);
 
 #if UART_PRINTF_MODE && DEBUG_BATTERY
     printf("Voltage_raw: %d\r\n", voltage_raw);
+    printf("Voltage_9v:  %d\r\n", voltage_9v);
     printf("Voltage:     %d\r\n", voltage);
     printf("Level:       %d\r\n", level);
 #endif
@@ -37,8 +114,35 @@ int32_t batteryCb(void *arg) {
     zcl_setAttrVal(APP_ENDPOINT1, ZCL_CLUSTER_GEN_POWER_CFG, ZCL_ATTRID_BATTERY_VOLTAGE, &voltage);
     zcl_setAttrVal(APP_ENDPOINT1, ZCL_CLUSTER_GEN_POWER_CFG, ZCL_ATTRID_BATTERY_PERCENTAGE_REMAINING, &level);
 
-    drv_adc_enable(OFF);
-    voltage_divisor_en(OFF);
+    fillIASAddress(&dstEpInfo);
+
+    zcl_getAttrVal(APP_ENDPOINT1, ZCL_CLUSTER_SS_IAS_ZONE, ZCL_ATTRID_ZONE_STATUS, &len, (uint8_t*)&statusChangeNotification.zoneStatus);
+    zcl_getAttrVal(APP_ENDPOINT1, ZCL_CLUSTER_SS_IAS_ZONE, ZCL_ATTRID_ZONE_ID, &len, &statusChangeNotification.zoneId);
+
+    if (voltage_9v < MIN_VBAT_MV) {
+
+        if (!(statusChangeNotification.zoneStatus & ZONE_STATUS_BIT_BATTERY)) {
+
+            statusChangeNotification.zoneStatus |= ZONE_STATUS_BIT_BATTERY;
+            zcl_setAttrVal(APP_ENDPOINT1, ZCL_CLUSTER_SS_IAS_ZONE, ZCL_ATTRID_ZONE_STATUS, (uint8_t*)&statusChangeNotification.zoneStatus);
+            statusChangeNotification.extStatus = 0;
+            statusChangeNotification.delay = 0;
+
+            zcl_iasZone_statusChangeNotificationCmd(APP_ENDPOINT1, &dstEpInfo, TRUE, &statusChangeNotification);
+        }
+
+    } else {
+
+       if (statusChangeNotification.zoneStatus & ZONE_STATUS_BIT_BATTERY) {
+
+           statusChangeNotification.zoneStatus &= ~ZONE_STATUS_BIT_BATTERY;
+           zcl_setAttrVal(APP_ENDPOINT1, ZCL_CLUSTER_SS_IAS_ZONE, ZCL_ATTRID_ZONE_STATUS, (uint8_t*)&statusChangeNotification.zoneStatus);
+           statusChangeNotification.extStatus = 0;
+           statusChangeNotification.delay = 0;
+
+           zcl_iasZone_statusChangeNotificationCmd(APP_ENDPOINT1, &dstEpInfo, TRUE, &statusChangeNotification);
+       }
+    }
 
     return 0;
 }
