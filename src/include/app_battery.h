1e3c2133a506d013ae91dee58ae65f279f77ae86
Fedor <lfa1@yandex.ru>
2024-07-05 07:17:08 +0300

    Update app_battery.h

--- src/include/app_battery.h
+++ src/include/app_battery.h
@@ -1,9 +1,9 @@
 #ifndef SRC_INCLUDE_APP_BATTERY_H_
 #define SRC_INCLUDE_APP_BATTERY_H_
 
-#define MAX_VBAT_MV             3100                        /* 3100 mV - > battery = 100%         */
-#define MIN_VBAT_MV             BATTERY_SAFETY_THRESHOLD    /* 2200 mV - > battery = 0%           */
-#define BATTERY_TIMER_INTERVAL  TIMEOUT_15MIN
+#define MAX_VBAT_MV             9000            /* 9000 mV - > battery = 100%         */
+#define MIN_VBAT_MV             7000            /* 7000 mV - > battery = 0%           */
+#define BATTERY_TIMER_INTERVAL  TIMEOUT_1HOUR
 
 int32_t batteryCb(void *arg);
 void battery_init();
