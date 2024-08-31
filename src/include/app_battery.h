8946280dc9a0727dd3d4c2bb9c6c74b1dc0e5e68
Fedor <lfa1@yandex.ru>
2024-08-31 14:37:33 +0300

    Update app_battery.h

--- src/include/app_battery.h
+++ src/include/app_battery.h
@@ -5,8 +5,6 @@
 #define MIN_VBAT_MV             7000            /* 7000 mV - > battery = 0%           */
 #define BATTERY_TIMER_INTERVAL  TIMEOUT_1HOUR
 
-#define ADC_CALIBRATION_ID      0x1965
-
 typedef struct __attribute__((packed)) _adc_calibration_t {
     uint16_t    id;                             /* 0x1965                             */
     uint8_t     voltage_factor;                 /* 1-255; default - 0x78 (120)        */
